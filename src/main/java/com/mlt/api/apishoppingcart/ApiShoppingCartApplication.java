2fe89cf767bf96f1ee0885c3e06b9c6ad2915390
RustyCrazyPunky <crazypunky@protonmail.com>
2023-08-27 01:01:07 -0300

    Init

---
+++ src/main/java/com/mlt/api/apishoppingcart/ApiShoppingCartApplication.java
@@ -0,0 +1,13 @@
+package com.mlt.api.apishoppingcart;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiShoppingCartApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiShoppingCartApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/mlt/api/apishoppingcart/ApiShoppingCartApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mlt.api.apishoppingcart;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiShoppingCartApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
